7d78b93f22ac139ece7078ef8d0143b632df3f97
Jeraldheo <jeyson.marin@unah.hn>
2023-09-15 12:35:24 -0600

    Added Test_Equal_Matrices_Return_Nine to check that nine is return when comparing the same matrix.

--- ComparingMatrices/Program.cs
+++ ComparingMatrices/Program.cs
@@ -26,5 +26,21 @@ namespace ComparingMatrices
             }
             return count;
         }
+
+        public static int CountEqualElements(int[,] matrix1, int[,] matrix2)
+        {
+            int count = 0;
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix2.GetLength(1); j++)
+                {
+                    if (matrix1[i, j] == matrix2[i, j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
     }
 }
---
+++ TestComparingMatrices/TestCountEqualElements.cs
@@ -0,0 +1,35 @@
+﻿using ComparingMatrices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestComparingMatrices
+{
+    public class TestCountEqualElements
+    {
+        public static IEnumerable<object[]> ExpectedCount_Matrix
+            => new[]
+            {
+                new object[] { 9, new int[,]{ {0, 0, 0}, {0, 0, 0}, { 0, 0, 0 } } },
+                new object[] { 9, new int[,]{ {1, 1, 1}, {1, 1, 1}, { 1, 1, 1 } } },
+                new object[] { 9, new int[,]{ {int.MaxValue, int.MaxValue, int.MaxValue },
+                                              {int.MaxValue, int.MaxValue, int.MaxValue },
+                                              {int.MaxValue, int.MaxValue, int.MaxValue } }},
+
+                new object[] { 9, new int[,]{ {int.MinValue, int.MinValue, int.MinValue },
+                                              {int.MinValue, int.MinValue, int.MinValue },
+                                              {int.MinValue, int.MinValue, int.MinValue } }}
+
+            };
+
+        [Theory]
+        [MemberData(nameof(ExpectedCount_Matrix))]
+        public void Test_Equal_Matrices_Return_Nine(int expectedCount, int[,] matrix)
+        {
+            int count = Program.CountEqualElements(matrix, matrix);
+            Assert.Equal(expectedCount, count);
+        }
+    }
+}
