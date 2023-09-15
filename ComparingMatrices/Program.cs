02613b928afcadd1b3cbf6c896d5620101cde08a
Jeraldheo <jeyson.marin@unah.hn>
2023-09-15 09:26:24 -0600

    Add project files.

---
+++ ComparingMatrices/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Collections.Immutable;
+
+namespace ComparingMatrices
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static int CountGreaterElements(int[,] matrix1, int[,] matrix2)
+        {
+            int count = 0;
+            for(int i = 0; i<matrix1.GetLength(0); i++)
+            {
+                for(int j = 0; j<matrix2.GetLength(1); j++)
+                {
+                    if (matrix1[i, j] > matrix2[i,j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
